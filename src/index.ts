9a93e78f849081b6b9774dad744091e851aa675f
Felix Novovic <felix.novovic@gmail.com>
2020-04-25 19:17:11 +0200

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,6 @@
+
+function getMessage(): string {
+    return "Hello world"
+}
+
+console.log(getMessage())
---
+++ webpack.config.js
@@ -0,0 +1,42 @@
+const path = require("path")
+const HtmlWebpackPlugin = require("html-webpack-plugin")
+const { CleanWebpackPlugin } = require("clean-webpack-plugin")
+
+module.exports = {
+    mode: "development",
+    entry: "./src/index.ts",
+    plugins: [
+        new CleanWebpackPlugin(),
+        new HtmlWebpackPlugin({
+            template: "./index.ejs"
+        }),
+    ],
+    devServer: {
+        contentBase: path.join(__dirname, "dist"),
+        compress: true,
+        port: 9000
+    },
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: "ts-loader",
+                exclude: /node_modules/,
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    "style-loader",
+                    "css-loader",
+                ],
+            },
+        ],
+    },
+    resolve: {
+        extensions: [".tsx", ".ts", ".js"],
+    },
+    output: {
+        filename: "bundle.js",
+        path: path.resolve(__dirname, "dist"),
+    },
+};
